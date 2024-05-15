c25dfd2599884a7e61d90860adf09a73278a0a0a
stevensusas <hanqisu@sas.upenn.edu>
2024-05-15 22:50:37 +0800

    venn diagram

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('circle1').addEventListener('click', function() {
+    window.location.href = 'page1.html';
+});
+
+document.getElementById('circle2').addEventListener('click', function() {
+    window.location.href = 'page2.html';
+});
+
+document.getElementById('intersection').addEventListener('click', function() {
+    window.location.href = 'intersection.html';
+});
